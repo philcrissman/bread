862f6c59f71044e106bad78c3d4ea02d799fc4a2
philcrissman <phil.crissman@gmail.com>
2009-02-15 00:50:14 -0600

    Added tiny_mce

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  uses_tiny_mce :only => [:new, :edit], :options => { :theme => 'advanced', :theme_advanced_toolbar_location => "top",
+  :theme_advanced_toolbar_align => "left" }
   
   def index
     @posts = Post.find( :all, :order => 'created_at DESC' )# well, all published.
@@ -9,11 +11,22 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.create(params[:post])
+    @post.user = current_user
     @post.save
     redirect_to root_url
   end
   
   def show
     @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes!(params[:post])
+    redirect_to post_url(@post)
   end
 end
