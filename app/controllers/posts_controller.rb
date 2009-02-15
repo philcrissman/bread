c78a02e196f6870cf77690e76676cc1d77f0ce81
philcrissman <phil.crissman@gmail.com>
2009-02-14 23:51:25 -0600

    Adding post CRUD, taggable plugin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.find :all # well, all published.
   end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.create(params[:post])
+    @post.save
+    redirect_to root_url
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,9 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "thoughtbot-clearance", :lib => "clearance", :source => "http://gems.github.com"
-
+  
+  config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
