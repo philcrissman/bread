0baffa16a0dbf957d7d244f7ab03584a0b1b793b
philcrissman <phil.crissman@gmail.com>
2009-02-15 23:35:39 -0600

    Added liquid gem for templating.

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,7 @@ Rails::Initializer.run do |config|
   
   config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
   
+  config.gem "liquid"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
