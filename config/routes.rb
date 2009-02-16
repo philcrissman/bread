d2f5895425b98f04c2e92b5061fd04251aa1dcab
philcrissman <phil.crissman@gmail.com>
2009-02-15 23:28:12 -0600

    Added easier routes for login, logout. Added .gitignore.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users, :has_one => [:password, :confirmation]
   map.resource :session
   map.resources :passwords
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   
   map.root :controller => 'posts', :action => 'index'
   map.resources :posts 
