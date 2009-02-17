f66330f63b3196788e01e194cb46209de2a1cb0e
philcrissman <phil.crissman@gmail.com>
2009-02-16 20:28:54 -0600

    Added jRails, and a theme.css file.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,7 +4,7 @@
 class ApplicationController < ActionController::Base
   include Clearance::App::Controllers::ApplicationController
   helper :all # include all helpers, all the time
-
+  
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'a0b3d8ae1a10f8368811fcb1abf1b2ae'
